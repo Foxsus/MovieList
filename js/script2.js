8c44b48d50f122074fef35e0874cac7ad7533719
RonanFelipe <ronan.nascimento@sga.pucminas.br>
2018-09-20 20:29:26 -0300

    javascript para mostrar detalhes

---
+++ js/script2.js
@@ -0,0 +1,7 @@
+function detailRedeSocial() {
+    var text = document.getElementById("Movie");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
