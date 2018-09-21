7fa60c012f0b044284cd1892cb539364a52df7d5
RonanFelipe <ronan.nascimento@sga.pucminas.br>
2018-09-21 20:42:25 -0300

    Actions details em cada card da página de trilha sonora.

--- js/script2.js
+++ js/script2.js
@@ -21,3 +21,107 @@ function detailArtist() {
     else
         text.style.display = "none";
 }
+
+function detailFilme4() {
+    var text = document.getElementById("Movie4");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme5() {
+    var text = document.getElementById("Movie5");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme6() {
+    var text = document.getElementById("Movie6");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme7() {
+    var text = document.getElementById("Movie7");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme8() {
+    var text = document.getElementById("Movie8");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme9() {
+    var text = document.getElementById("Movie9");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme10() {
+    var text = document.getElementById("Movie10");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme11() {
+    var text = document.getElementById("Movie11");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme12() {
+    var text = document.getElementById("Movie12");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme13() {
+    var text = document.getElementById("Movie13");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme14() {
+    var text = document.getElementById("Movie14");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme15() {
+    var text = document.getElementById("Movie15");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme16() {
+    var text = document.getElementById("Movie16");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
